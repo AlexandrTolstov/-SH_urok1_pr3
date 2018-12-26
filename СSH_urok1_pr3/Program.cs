c45ce71c7da28ee9abef7e93b6fca6e33f08e63b
AlexandrTolstov <alextav_85@mail.ru>
2018-12-26 20:45:51 +0600

    Add project files.

---
+++ СSH_urok1_pr3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СSH_urok1_pr3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
