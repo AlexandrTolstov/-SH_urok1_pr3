6afaba6bbfa5732bc787fe666d955510e9845535
AlexandrTolstov <alextav_85@mail.ru>
2018-12-26 20:53:01 +0600

    1

--- СSH_urok1_pr3/Program.cs
+++ СSH_urok1_pr3/Program.cs
@@ -10,6 +10,25 @@ namespace СSH_urok1_pr3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(char.IsControl('\t'));
+            Console.WriteLine(char.IsDigit('5'));
+            Console.WriteLine(char.IsLetter('x'));
+            Console.WriteLine(char.IsLower('m'));
+            Console.WriteLine(char.IsUpper('P')); //True
+                                                  //определяет является ли символ числом
+            Console.WriteLine(char.IsNumber('2')); //True
+                                                   //определяет является ли символ
+                                                   //разделителем
+            Console.WriteLine(char.IsSeparator('.')); //False
+                                                      //определяет является ли символ
+                                                      //специальным символом
+            Console.WriteLine(char.IsSymbol('<')); //True
+                                                   //определяет является ли символ пробелом
+            Console.WriteLine(char.IsWhiteSpace(' ')); //True
+                                                       //переводит символ в нижний регистр
+            Console.WriteLine(char.ToLower('T')); //t
+                                                  //переводит символ в верхний регистр
+            Console.WriteLine(char.ToUpper('t'));
         }
     }
 }
